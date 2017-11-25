bc10ba0efff794395fb4016836b62a1d944833dd
liuhui <lucifer>
2017-11-25 13:48:13 +0800

    hello world

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from '../style/index.css'
+import styles from '../style/index.less'
 
 class IndexComponent extends React.Component {
     render() {
         return <h1>hello world!!!</h1>
     }
 }
-var oBox = document.getElementById("box");
-
-
-ReactDOM.render(<IndexComponent />, oBox)
+ReactDOM.render(<IndexComponent />, document.getElementById("react-content"))
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: "./src/apps/index.js" //在源文件目录下去找index.js 文件作为打包的入口文件
+    },
+    output: {
+        path: path.resolve(__dirname + "/dist"), //生成的文件存放目录
+        filename: "bundle-[name]-[hash:5].js",
+        chunkFilename: "bundle-[name]-[hash:5].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/, exclude: /node_modules/, use: [{
+                    loader: "babel-loader?cacheDirectory=true", options: { presets: ["react", "es2015"] }
+                }]
+            },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
+            { test: /\.less$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' }) },
+            { test: /\.(png|gif)$/, loader: 'file-loader' }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'bundle-[name]-[hash:5].css', disable: false, allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.ejs'
+        })
+    ],
+    devServer: {
+        port: 8088,
+        disableHostCheck: true
+    }
+}

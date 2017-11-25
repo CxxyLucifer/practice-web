c19673cfc714ed71eb6bae6e68aa9d766eb8aa86
liuhui <lucifer>
2017-11-25 11:49:14 +0800

    project init

---
+++ src/apps/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../style/index.css'
+
+class IndexComponent extends React.Component {
+    render() {
+        return <h1>hello world!!!</h1>
+    }
+}
+var oBox = document.getElementById("box");
+
+
+ReactDOM.render(<IndexComponent />, oBox)
